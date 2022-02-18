105dcebd1aa5a365192b4a8568f95eea08562cce
Funakoshi Silva <fsilva@avfuel.com>
2022-02-18 12:48:32 -0500

    Warrior app basic setup

---
+++ App.Domain/Quote.cs
@@ -0,0 +1,10 @@
+﻿namespace App.Domain
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public Warrior Warrior { get; set; }
+        public int WarriorId { get; set; }
+    }
+}
---
+++ App.Domain/Warrior.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain
+{
+    public class Warrior
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Quote> Quotes { get; set; } = new List<Quote>();
+    }
+}
